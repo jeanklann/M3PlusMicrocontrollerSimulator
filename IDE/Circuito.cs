69dd7f7756f70d4b988298c21d05b18ee2009465
jeanklann <jeanklann@hotmail.com>
2017-03-10 00:04:50 -0300

    Feito os fios (ainda pendente algumas coisas) em que ao selecionar um terminal e arrastar ele cria o fio, assim como a seleção deles (quanto tem um angulo muuuito íngreme na vertical ele não funciona muito bem, verificar). Ajustado também alguns bugs gráficos, como a rotação correta do terminal, e a bounding box (visual) correta.

--- IDE/Circuito.cs
+++ IDE/Circuito.cs
@@ -13,8 +13,13 @@ using OpenTK.Graphics.OpenGL;
 namespace IDE {
     public partial class Circuito : GLControl {
         public MouseProps MouseProps = new MouseProps();
+        public Terminals Terminals = new Terminals();
+        public List<Wire> Wires = new List<Wire>();
+        public Wire HoverWire;
+        public Wire SelectedWire;
         public Component Over;
         public Component Selected;
+        
 
         public PointF Position = new PointF();
         public List<Component> Components = new List<Component>();
@@ -53,15 +58,29 @@ namespace IDE {
             if (MouseProps.Button2Pressed) {
                 Position = new PointF(Position.X+((MouseProps.LastPosition.X - MouseProps.CurrentPosition.X)/zoom), Position.Y-((MouseProps.LastPosition.Y - MouseProps.CurrentPosition.Y)/zoom));
             }
-            bool found = false;
+            bool foundComponent = false;
+            bool foundTerminal = false;
+
+            PointF worldMousePos = MouseProps.ToWorld(MouseProps.CurrentPosition, ClientSize, Position, zoom);
             foreach (Component item in Components) {
-                if(item.IsInside(MouseProps.ToWorld(MouseProps.CurrentPosition, ClientSize, Position, zoom))){
-                    found = true;
+                int index = item.IsOnTerminal(worldMousePos);
+                if (index >= 0) {
+                    Terminals.HoverIndex = index;
+                    Terminals.HoverComponent = item;
+                    Over = null;
+                    foundTerminal = true;
+                    break;
+                }
+                if (item.IsInside(worldMousePos)){
                     Over = item;
+                    foundComponent = true;
                     break;
                 }
             }
-            if (!found) Over = null;
+            if (!foundComponent) Over = null;
+            if (!foundTerminal) {
+                Terminals.HoverComponent = null;
+            }
 
             if (MouseProps.Button1Pressed) {
                 if (((MouseProps.LastDownPosition.X - MouseProps.CurrentPosition.X) * (MouseProps.LastDownPosition.X - MouseProps.CurrentPosition.X) +
@@ -71,9 +90,41 @@ namespace IDE {
                         Selected.Center.Y += (MouseProps.LastPosition.Y - MouseProps.CurrentPosition.Y) / zoom;
                     }
                 }
+                if(Terminals.FromComponent != null) {
+                    terminalFromVertex = Terminals.FromComponent.TransformTerminal(Terminals.FromIndex);
+                    terminalFromVertex.X += Terminals.FromComponent.Center.X;
+                    terminalFromVertex.Y += Terminals.FromComponent.Center.Y;
+                    terminalToVertex = MouseProps.ToWorld(MouseProps.CurrentPosition, ClientSize, Position, zoom);
+                }
             }
+            bool foundWire = false;
+            
+            foreach (Wire item in Wires) {
+                if (item.FromComponent != null) {
+                    item.From = item.FromComponent.TransformTerminal(item.FromIndex);
+                    item.From.X += item.FromComponent.Center.X;
+                    item.From.Y += item.FromComponent.Center.Y;
+                }
+                if (item.ToComponent != null) {
+                    item.To = item.ToComponent.TransformTerminal(item.ToIndex);
+                    item.To.X += item.ToComponent.Center.X;
+                    item.To.Y += item.ToComponent.Center.Y;
+                }
+                
+                if (!MouseProps.Button1Pressed && !MouseProps.Button2Pressed) {
+                    if (OnWire(item.From, item.To, worldMousePos)) {
+                        HoverWire = item;
+                        foundWire = true;
+                        break;
+                    }
+                }
+            }
+            if (!foundWire) HoverWire = null;
+
+
         }
-        
+        private PointF terminalFromVertex;
+        private PointF terminalToVertex;
         private void Render() {
             Matrix4 lookat = Matrix4.LookAt(Position.X, Position.Y, 1, Position.X, Position.Y, 0, 0, 1, 0);
             GL.MatrixMode(MatrixMode.Modelview);
@@ -98,6 +149,16 @@ namespace IDE {
                 GL.Translate(-item.Center.X, -item.Center.Y, 0);
             }
 
+            if (MouseProps.Button1Pressed) {
+                if(Terminals.FromComponent != null) {
+                    GL.Color3(Color.Black);
+                    GL.Begin(BeginMode.Lines);
+                    GL.Vertex2(terminalFromVertex.X, terminalFromVertex.Y);
+                    GL.Vertex2(terminalToVertex.X, terminalToVertex.Y);
+                    GL.End();
+                }
+            }
+            DrawWires();
             DrawBoxes();
 
             SwapBuffers();
@@ -157,6 +218,18 @@ namespace IDE {
                     Selected = null;
                     if(Over != null)
                         Selected = Over;
+                    if(Terminals.HoverComponent != null) {
+                        Terminals.FromComponent = Terminals.HoverComponent;
+                        Terminals.FromIndex = Terminals.HoverIndex;
+                    } else {
+                        Terminals.FromComponent = null;
+                        Terminals.ToComponent = null;
+                    }
+                    if(HoverWire != null) {
+                        SelectedWire = HoverWire;
+                    } else {
+                        SelectedWire = null;
+                    }
                 } else {
                     MouseProps.Button2Pressed = true;
                 }
@@ -179,6 +252,15 @@ namespace IDE {
                     MouseProps.Button2Pressed = false;
                 }
             }
+            if (Terminals.FromComponent != null) {
+                if (Terminals.HoverComponent != null) {
+                    if (Terminals.HoverComponent != Terminals.FromComponent || Terminals.HoverIndex != Terminals.FromIndex) {
+                        Wires.Add(new Wire(Terminals.FromComponent, Terminals.FromIndex, Terminals.HoverComponent, Terminals.HoverIndex));
+                    }
+                } else {
+                    Wires.Add(new Wire(Terminals.FromComponent, Terminals.FromIndex, MouseProps.ToWorld(MouseProps.LastUpPosition, ClientSize, Position, zoom)));
+                }
+            }
             Refresh();
         }
 
@@ -189,30 +271,65 @@ namespace IDE {
             */
             Refresh();
         }
+
+        private void DrawWires() {
+            foreach (Wire item in Wires) {
+                if (item == HoverWire) {
+                    GL.Color3(Color.Green);
+                } else if (item == SelectedWire) {
+                    GL.Color3(Color.Orange);
+                } else {
+                    GL.Color3(Color.Black);
+                }
+                GL.Begin(BeginMode.Lines);
+                GL.Vertex2(item.From.X, item.From.Y);
+                GL.Vertex2(item.To.X, item.To.Y);
+                GL.End();
+            }
+        }
         private const int BoxPlusFactor = 5;
         private void DrawBoxes() {
-            if(Selected != null) { 
+            if (Terminals.HoverComponent != null) {
+                GL.Color3(Color.Green);
+                Vector3 pos = new Vector3(Terminals.HoverComponent.Center.X, Terminals.HoverComponent.Center.Y, 0);
+                PointF transform = Terminals.HoverComponent.TransformTerminal(Terminals.HoverIndex);
+                pos.X += transform.X;
+                pos.Y += transform.Y;
+                GL.Translate(pos);
+                GL.CallList(Draws.TerminalHandle);
+                GL.Translate(-pos);
+            }
+            if (Selected != null) { 
                 GL.Color3(Color.Orange);
                 GL.LineWidth(1);
                 GL.LineStipple(1, 0xAAAA); // dashed line
+                GL.Translate(Selected.Center.X, Selected.Center.Y, 0);
+                GL.Rotate(Selected.Rotation, 0, 0, 1);
                 GL.Begin(BeginMode.LineLoop);
-                GL.Vertex2(Selected.Center.X - Selected.Draw.Width / 2f - BoxPlusFactor, Selected.Center.Y - Selected.Draw.Height / 2f - BoxPlusFactor);
-                GL.Vertex2(Selected.Center.X - Selected.Draw.Width / 2f - BoxPlusFactor, Selected.Center.Y + Selected.Draw.Height / 2f + BoxPlusFactor);
-                GL.Vertex2(Selected.Center.X + Selected.Draw.Width / 2f + BoxPlusFactor, Selected.Center.Y + Selected.Draw.Height / 2f + BoxPlusFactor);
-                GL.Vertex2(Selected.Center.X + Selected.Draw.Width / 2f + BoxPlusFactor, Selected.Center.Y - Selected.Draw.Height / 2f - BoxPlusFactor);
+                GL.Vertex2(- Selected.Draw.Width / 2f - BoxPlusFactor, - Selected.Draw.Height / 2f - BoxPlusFactor);
+                GL.Vertex2(- Selected.Draw.Width / 2f - BoxPlusFactor, Selected.Draw.Height / 2f + BoxPlusFactor);
+                GL.Vertex2(Selected.Draw.Width / 2f + BoxPlusFactor, Selected.Draw.Height / 2f + BoxPlusFactor);
+                GL.Vertex2(Selected.Draw.Width / 2f + BoxPlusFactor, - Selected.Draw.Height / 2f - BoxPlusFactor);
                 GL.End();
+                GL.Rotate(-Selected.Rotation, 0, 0, 1);
+                GL.Translate(-Selected.Center.X, -Selected.Center.Y, 0);
             }
             if(Over != null) {
                 GL.Color3(Color.Green);
                 GL.LineWidth(1);
                 GL.LineStipple(1, 0xAAAA); // dashed line
+                GL.Translate(Over.Center.X, Over.Center.Y, 0);
+                GL.Rotate(Over.Rotation, 0, 0, 1);
                 GL.Begin(BeginMode.LineLoop);
-                GL.Vertex2(Over.Center.X - Over.Draw.Width/2f - BoxPlusFactor, Over.Center.Y - Over.Draw.Height/ 2f - BoxPlusFactor);
-                GL.Vertex2(Over.Center.X - Over.Draw.Width / 2f - BoxPlusFactor, Over.Center.Y + Over.Draw.Height / 2f + BoxPlusFactor);
-                GL.Vertex2(Over.Center.X + Over.Draw.Width / 2f + BoxPlusFactor, Over.Center.Y + Over.Draw.Height / 2f + BoxPlusFactor);
-                GL.Vertex2(Over.Center.X + Over.Draw.Width / 2f + BoxPlusFactor, Over.Center.Y - Over.Draw.Height / 2f - BoxPlusFactor);
+                GL.Vertex2(- Over.Draw.Width/2f - BoxPlusFactor, - Over.Draw.Height/ 2f - BoxPlusFactor);
+                GL.Vertex2(- Over.Draw.Width / 2f - BoxPlusFactor, + Over.Draw.Height / 2f + BoxPlusFactor);
+                GL.Vertex2(Over.Draw.Width / 2f + BoxPlusFactor, Over.Draw.Height / 2f + BoxPlusFactor);
+                GL.Vertex2(Over.Draw.Width / 2f + BoxPlusFactor, - Over.Draw.Height / 2f - BoxPlusFactor);
                 GL.End();
+                GL.Rotate(-Over.Rotation, 0, 0, 1);
+                GL.Translate(-Over.Center.X, -Over.Center.Y, 0);
             }
+            
         }
 
         private void Circuito_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e) {
@@ -222,6 +339,63 @@ namespace IDE {
                     if (Selected.Rotation >= 360) Selected.Rotation -= 360;
                 }
             }
+            Refresh();
+        }
+
+        private static bool OnWire(PointF point1, PointF point2, PointF point) {
+            //Calculando a equação reta
+            float coefAng = (point2.Y - point1.Y) / (point2.X - point1.X);
+            float coefLin = -point1.X * coefAng + point1.Y;
+            //calculando a reta perpendicar passando no ponto
+            float coefAngPar = -1 / coefAng * coefLin;
+            float coefLinPar = point.Y - coefAngPar * point.X;
+            //calculando o ponto de interceção das duas retas
+            float x = (coefLin - coefLinPar) / (coefAngPar - coefAng);
+            float y = x * coefAng + coefLin;
+            //pegando os valores máximos
+            float maxX = point1.X > point2.X ? point1.X : point2.X;
+            float minX = point1.X > point2.X ? point2.X : point1.X;
+            float maxY = point1.Y > point2.Y ? point1.Y : point2.Y;
+            float minY = point1.Y > point2.Y ? point2.Y : point1.Y;
+            
+            
+            //verifica se o ponto Y passa dos limites da linha
+            if (y < minY) {
+                y = minY;
+                x = y / coefAng - coefLin;
+            } else if (y > maxY) {
+                y = maxY;
+                x = y / coefAng - coefLin;
+            }
+            
+
+            //verifica se o ponto X passa dos limites da linha
+            if (x < minX) {
+                x = minX;
+                y = x * coefAng + coefLin;
+            } else if (x > maxX) {
+                x = maxX;
+                y = x * coefAng + coefLin;
+            }
+
+            //Verifica se é uma reta totalmente paralela ao eixo y
+            if (float.IsNaN(x) || float.IsNaN(y)) {
+                x = point1.X;
+                y = point.Y > maxY ? maxY : (point.Y < minY ? minY : point.Y);
+            }
+
+            float maiorX = x > point.X ? x : point.X;
+            float menorX = x > point.X ? point.X : x;
+            float maiorY = y > point.Y ? y : point.Y;
+            float menorY = y > point.Y ? point.Y : y;
+
+            Console.WriteLine((maiorX - menorX) * (maiorX - menorX) + (maiorY - menorY) * (maiorY - menorY));
+
+            //calcula a distancia
+            if ((maiorX - menorX) * (maiorX - menorX) + (maiorY - menorY) * (maiorY - menorY) < 5 * 5) {
+                return true;
+            }
+            return false;
         }
     }
     public static class Draws {
@@ -343,15 +517,78 @@ namespace IDE {
             return rectangle.Contains(Point);
         }
 
+        public int IsOnTerminal(PointF Point) {
+            for (int i = 0; i < Draw.Terminals.Length; i++) {
+                PointF terminal = TransformTerminal(i);
+                float distance = (Center.X + terminal.X - Point.X) * (Center.X + terminal.X - Point.X) + (Center.Y + terminal.Y - Point.Y) * (Center.Y + terminal.Y - Point.Y);
+                if(distance <= 4*4) {
+                    return i;
+                }
+            }
+            
+            return -1;
+        }
+
         public PointF TransformTerminal(int i) {
+            double RotDegree = Math.PI * Rotation / 180.0;
             PointF point = new PointF(
-                (float)(Draw.Terminals[i].X * Math.Cos(Rotation) - Draw.Terminals[i].Y * Math.Sin(Rotation)),
-                (float)(Draw.Terminals[i].X * Math.Sin(Rotation) + Draw.Terminals[i].Y * Math.Cos(Rotation))
+                (float)(Draw.Terminals[i].X * Math.Cos(RotDegree) - Draw.Terminals[i].Y * Math.Sin(RotDegree)),
+                (float)(Draw.Terminals[i].X * Math.Sin(RotDegree) + Draw.Terminals[i].Y * Math.Cos(RotDegree))
             );
             return point;
         }
-    }
 
+    }
+    public class Wire {
+        public PointF From;
+        public Component FromComponent;
+        public int FromIndex;
+
+        public PointF To;
+        public Component ToComponent;
+        public int ToIndex;
+
+        public Wire(PointF from, PointF to) {
+            From = from;
+            To = to;
+        }
+        public Wire(Component from, int indexFrom, Component to, int indexTo) {
+            FromComponent = from;
+            FromIndex = indexFrom;
+            ToComponent = to;
+            ToIndex = indexTo;
+            From = from.TransformTerminal(indexFrom);
+            From.X += from.Center.X;
+            From.Y += from.Center.Y;
+            To = to.TransformTerminal(indexTo);
+            To.X += to.Center.X;
+            To.Y += to.Center.Y;
+        }
+        public Wire(Component from, int indexFrom, PointF to) {
+            FromComponent = from;
+            FromIndex = indexFrom;
+            From = from.TransformTerminal(indexFrom);
+            From.X += from.Center.X;
+            From.Y += from.Center.Y;
+            To = to;
+        }
+        public Wire(PointF from, Component to, int indexTo) {
+            From = from;
+            ToComponent = to;
+            ToIndex = indexTo;
+            To = to.TransformTerminal(indexTo);
+            To.X += to.Center.X;
+            To.Y += to.Center.Y;
+        }
+    }
+    public struct Terminals {
+        public int FromIndex;
+        public Component FromComponent;
+        public int ToIndex;
+        public Component ToComponent;
+        public int HoverIndex;
+        public Component HoverComponent;
+    }
     public struct MouseProps {
         public Point CurrentPosition;
         public Point LastPosition;
